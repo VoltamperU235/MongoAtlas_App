5f1cdea4c4dad27c4502138d21998fbd1444d139
VoltamperU235 <denirevolver@gmail.com>
2019-08-23 08:51:02 -0600

    first commit with support for Heroku and Google App engine

---
+++ MongoAtlas_App/public/js/mupi.js
@@ -0,0 +1,9 @@
+$(this).mousemove(function (e) {
+    window.location.href = "http://localhost/index.html";
+
+});
+$(this).keypress(function (e) {
+    window.location.href = "http://localhost/index.html";
+
+});
+
